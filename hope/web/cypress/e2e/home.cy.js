614d0ff8de304f19a160310f32ffe9e257b96043
cedric arnaud <79386450+CedricArnaud20@users.noreply.github.com>
2024-05-08 11:31:55 -0300

    Commit novo projeto
    MoveBee

--- hope/web/cypress/e2e/home.cy.js
+++
@@ -1,8 +0,0 @@
-describe('home spec', () => {
-  it('hope web deve estar online', () => {
-    cy.visit('/')
-
-    cy.get('h1')
-      .should('have.text', 'Semeando esperança, colhendo sorrisos')
-  })
-})
